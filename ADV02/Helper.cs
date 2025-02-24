e4f9d8946f83a3e7e78e72b2ccdeae352328d4bf
Fahmy Ramadan <fahmyramadan22@gmail.com>
2025-02-25 00:14:30 +0200

    Add project files.

---
+++ ADV02/Helper.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV02
+{
+    internal static class Helper
+    {
+        #region Question 01
+
+        public static void PrintCollection(IEnumerable collection)
+        {
+            foreach (var item in collection)
+                Console.Write(item + " ");
+
+            Console.WriteLine();
+
+        }
+
+
+        public static void PrintGreaterThanValue(int[] arr, int[] queries)
+        {
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                    if (arr[j] > queries[i])
+                        count++;
+
+                Console.WriteLine($"Number of elements greater than {queries[i]} is: {count}");
+            }
+        }
+
+
+        #endregion
+
+        #region Question 02
+
+        public static bool IsPlaindrome(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                if (numbers[i] != numbers[numbers.Length - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+
+       
+        public static bool IsPlindrome2(int[] numbers)
+        {
+
+            return numbers.SequenceEqual(numbers.Reverse());
+        }
+
+
+
+        #endregion
+
+        #region Question 03
+
+        public static Queue<T> ReverseQueueUsingStack<T>(Queue<T> queue)
+        {
+            Stack<T> tempStack = new Stack<T>();
+
+            while (queue.Count > 0)
+                tempStack.Push(queue.Dequeue());
+
+            while (tempStack.Count > 0)
+                queue.Enqueue(tempStack.Pop());
+
+            return queue;
+
+
+        }
+
+        #endregion
+
+        #region Question 04
+
+        public static bool IsMatchedPair(char Open, char Closed)
+        {
+            if (Open == '(' && Closed == ')')
+                return true;
+            else if (Open == '{' && Closed == '}')
+                return true;
+            else if (Open == '[' && Closed == ']')
+                return true;
+
+            return false;
+        }
+
+
+        public static bool IsBalancedString(string expression)
+        {
+            Stack<char> tempStack = new Stack<char>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(' || expression[i] == '[' || expression[i] == '{')
+                    tempStack.Push(expression[i]);
+                else if (expression[i] == ')' || expression[i] == ']' || expression[i] == '}')
+                {
+                    if (tempStack.Count == 0 || !IsMatchedPair(tempStack.Peek(), expression[i]))
+                        return false;
+                    else
+                        tempStack.Pop();
+                }
+
+            }
+
+            return tempStack.Count == 0;
+        }
+        #endregion
+
+        #region Question 05
+
+        public static T[] RemoveDuplicates<T>(T[] arr) where T : struct
+        {
+            if (arr.Length == 0)
+                return arr;
+
+        
+
+
+            Array.Sort(arr);
+
+            T[] tempArray = new T[arr.Length];
+            tempArray[0] = arr[0];
+            int count = 1;
+
+
+            for (int i = 1; i < arr.Length; i++)
+                if (!arr[i].Equals(arr[i - 1]))
+
+                    
+
+                    tempArray[count++] = arr[i];  
+
+            T[] result = new T[count]; // 4
+            Array.Copy(tempArray, result, count);
+
+            return result;
+
+        }
+
+
+        #endregion
+
+        #region Question 06
+
+        public static void RemoveOddNumbers(ArrayList list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((int?)list[i] % 2 != 0)
+                {
+
+                    list.RemoveAt(i);
+                }
+
+            }
+        }
+
+
+        #endregion
+
+        #region Question 08
+
+        public static void FindTargetInStack(Stack<int> stack, int target)
+        {
+            bool found = false;
+            int count = 0;
+
+            foreach (var item in stack)
+            {
+                if (item == target)
+                {
+                    found = true;
+                    break;
+                }
+
+                count++;
+
+            }
+
+            if (found)
+                Console.WriteLine($"Target is found and the count is {count}");
+            else
+                Console.WriteLine("Target is not found");
+
+
+
+
+        }
+
+        #endregion
+
+        #region Question 09
+        public static T[] FindInteresection<T>(T[] arr1, T[] arr2) where T : struct, IComparable
+        {
+
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+
+            List<T> intersectionList = [];
+
+            int i = 0, j = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i].CompareTo(arr2[j]) == -1)
+                    i++;
+                else if (arr1[i].CompareTo(arr2[j]) == 1)
+                    j++;
+                else
+                {
+                    
+                    intersectionList.Add(arr1[i]);
+                    i++;
+                    j++;
+
+
+                }
+            }
+
+            return intersectionList.ToArray();
+
+        }
+
+
+
+        #endregion
+
+        #region Question 10
+        public static int[]? FindContSubArray(ArrayList arrayList, int target)
+        {
+            if (arrayList.Count == 0)
+                return null;
+
+            int n = arrayList.Count;
+
+            int[] arr = (int[])arrayList.ToArray(typeof(int));
+
+         
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentSum = arr[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    currentSum += arr[j];
+                    if (currentSum == target)
+                    {
+                        int[] result = new int[j - i + 1];
+                        Array.Copy(arr, i, result, 0, j - i + 1);
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Question 11
+
+        public static Queue<int>? ReverseKElements(Queue<int> queue, int k)
+        {
+            if (k <= 0 || k >= queue.Count)
+            {
+                Console.WriteLine("Invalid K value");
+                return null;
+            }
+
+            Stack<int> tempStack = new Stack<int>();
+
+            for (int i = 0; i < k; i++)
+                tempStack.Push(queue.Dequeue());
+
+            for (int i = 0; i < k; i++)
+                queue.Enqueue(tempStack.Pop());
+
+            for (int i = 0; i < queue.Count - k; i++)
+                queue.Enqueue(queue.Dequeue());
+
+            return queue;
+
+
+        }
+
+        #endregion
+    }
+
+}
+
---
+++ ADV02/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+
+namespace ADV02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
+
+
+            //int arrSize, queriesNumber;
+
+            //do
+            //{
+            //    Console.Write("Enter the size of the array: ");
+            //} while (!int.TryParse(Console.ReadLine(), out arrSize));
+
+            //do
+            //{
+            //    Console.Write("Enter the number of queries: ");
+            //} while (!int.TryParse(Console.ReadLine(), out queriesNumber));
+
+            //int[] arr = new int[arrSize];
+
+            //Console.WriteLine("Enter the elements of the array");
+
+            //for (int i = 0; i < arrSize; i++)
+            //{
+            //    do
+            //    {
+            //        Console.Write($"Enter the element number {i + 1}: ");
+            //    } while (!int.TryParse(Console.ReadLine(), out arr[i]));
+            //}
+
+            //int[] queries = new int[queriesNumber];
+
+            //for (int i = 0; i < queriesNumber; i++)
+            //{
+            //    do
+            //    {
+            //        Console.Write($"Enter the query number {i + 1}: ");
+            //    } while (!int.TryParse(Console.ReadLine(), out queries[i]));
+            //}
+
+            //Helper.PrintGreaterThanValue(arr, queries);
+
+            #endregion
+            #region 2. Given a number N and an array of N numbers. Determine if it's palindrome or not.
+
+            //int[] numbers = [1, 3, 2, 3, 1];
+
+            //bool isPlaindrome = Helper.IsPlaindrome(numbers);
+
+            //if (isPlaindrome)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+            //isPlaindrome = Helper.IsPlindrome2(numbers);
+
+            //if (isPlaindrome)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+            #endregion
+            #region 3. Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+
+            //Queue<int> queue = new([1, 2, 3, 4]);
+            //Queue<int> reversedQueue = Helper.ReverseQueueUsingStack(queue);
+
+            //foreach (var item in reversedQueue)
+            //    Console.Write(item + " ");
+            #endregion
+            #region 4. Given a Stack, implement a function to check if a string of parentheses is balanced using a stack.
+
+            //string str = "[()]{}";
+            //bool isBalanced = Helper.IsBalancedString(str);
+
+            //if (isBalanced)
+            //    Console.WriteLine("Balanced");
+            //else
+            //    Console.WriteLine("Not Balanced");
+
+            #endregion
+            #region 5. Given an array, implement a function to remove duplicate elements from an array.
+
+            //int[] arr = [1, 4, 4, 2, 5, 2];
+            //int[] result = Helper.RemoveDuplicates(arr);
+
+            //Helper.PrintCollection(result);
+
+            #endregion
+            #region 6. Given an array list , implement a function to remove all odd numbers from it.
+
+            //ArrayList list = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Helper.RemoveOddNumbers(list);
+
+            //Helper.PrintCollection(list);
+
+            #endregion
+            #region 7. Implement a queue that can hold different data types. 
+
+
+            //Queue queue = new();
+
+            //queue.Enqueue(1);
+            //queue.Enqueue("Apple");
+            //queue.Enqueue(5.28);
+
+            //Helper.PrintCollection(queue);
+
+
+            #endregion
+            #region 8. Create a function that pushes a series of integers onto a stack. Then, search for a target integer in the stack. If the target is found, print a message indicating that the target was found how many elements were checked before finding the target (“Target was found successfully and the count = 5”). If the target is not found, print a message indicating that the target was not found(“Target was not found”).
+
+            //Stack<int> numbers = new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+            //int target = 5;
+
+            //Helper.FindTargetInStack(numbers, target);
+            #endregion
+            #region 9. Given two arrays, find their intersection. Each element in the result should appear as many times as it shows in both arrays.
+
+            //int[] arr1 = [1, 2, 3, 4, 4];
+            //int[] arr2 = [10, 4, 4];
+
+            //int[] result = Helper.FindInteresection(arr1, arr2);
+
+            //Helper.PrintCollection(result);
+
+            //char[] arr3 = ['a', 'b', 'b', 'c', 'd', 'c'];
+            //char[] arr4 = ['b', 'b', 'c', 'd', 'c'];
+
+            //char[] result2 = Helper.FindInteresection(arr3, arr4);
+
+            //Helper.PrintCollection(result2);
+            #endregion
+            #region 10. Given an ArrayList of integers and a target sum, find if there is a contiguous sub list that sums up to the target.
+
+            //ArrayList arrayList = [1, 2, 3, 7, 5];
+            //int target = 12;
+
+            //int[] result = Helper.FindContSubArray(arrayList, target)!;
+
+            //Helper.PrintCollection(result);
+
+            #endregion
+            #region 11. Given a queue reverse first K elements of a queue, keeping the remaining elements in the same order 
+
+            //Queue<int> queue = new Queue<int>([1, 2, 3, 4, 5]);
+            //int k = 3;
+
+            //Queue<int> result = Helper.ReverseKElements(queue, k)!;
+
+            //Helper.PrintCollection(result);
+
+            #endregion
+        }
+    }
+}
